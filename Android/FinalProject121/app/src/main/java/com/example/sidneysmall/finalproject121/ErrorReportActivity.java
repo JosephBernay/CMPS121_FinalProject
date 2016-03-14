6dd257dfcf5f2c61876f6c4d4b49fc01428146e8
AndrewParvisUCSC <aparvis@ucsc.edu>
2016-03-14 16:25:29 -0700

    Posting Error Reports to Server
    ^ Need I say more?

--- Android/FinalProject121/app/src/main/java/com/example/sidneysmall/finalproject121/ErrorReportActivity.java
+++ Android/FinalProject121/app/src/main/java/com/example/sidneysmall/finalproject121/ErrorReportActivity.java
@@ -13,6 +13,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.sidneysmall.finalproject121.response.AddUserResponse;
+import com.example.sidneysmall.finalproject121.response.GetUserResponse;
+
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -24,24 +27,35 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public class ErrorReportActivity extends AppCompatActivity {
 
     String LOG = "Print: ";
 
-    String[] stuff;
     String email;
     String comp;
     String room;
     Session session = null;
     String rec, subject, textMessage;
     String error;
+    String key;
+    String details;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_error_report);
-        Spinner dropdown = (Spinner)findViewById(R.id.spinner);
+        Spinner dropdown = (Spinner) findViewById(R.id.spinner);
         SpinnerActivity spinnerActivity = new SpinnerActivity();
         dropdown.setOnItemSelectedListener(spinnerActivity);
         String[] items = new String[]{"Select One", "Randomly Shuts Down", "Display Driver Failure", "Graphics Card Failure", "Randomly Enters Power Save Mode", "Other"};
@@ -51,56 +65,89 @@ public class ErrorReportActivity extends AppCompatActivity {
         email = intent.getStringExtra("email");
         comp = intent.getStringExtra("currentComputer");
         room = intent.getStringExtra("roomNumber");
-
+        key = getString(R.string.KEY);
     }
 
-    public void back(View v){
+    public void back(View v) {
         Intent myIntent = new Intent(ErrorReportActivity.this, LabView.class);
         startActivity(myIntent);
     }
 
-    public void send(View v){
-        if(error != null && !error.equals("Select One")){
-            TextView dets = (TextView)findViewById(R.id.editText);
-            String details = dets.getText().toString();
-            rec = "aparvis@ucsc.edu";
-            subject = "GLCS: Error Report";
-            textMessage = "Computer: " + comp + "Room: " + room + "Error: " + error + "Details: " + details;
-            Properties props = new Properties();
-            props.put("mail.smtp.host", "smtp.gmail.com");
-            props.put("mail.smtp.socketFactory.port", "465");
-            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.port", "465");
+    public void send(View v) {
+        if (error != null && !error.equals("Select One")) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient httpClient = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
 
-            session = Session.getDefaultInstance(props, new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("glcs.ucsc@gmail.com", "Okay. What now, Lauren?");
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("http://lauren.pythonanywhere.com/welcome/default/")
+                    .addConverterFactory(GsonConverterFactory.create())    //parse Gson string
+                    .client(httpClient)    //add logging
+                    .build();
+
+            PostMessageService service = retrofit.create(PostMessageService.class);
+
+            TextView dets = (TextView) findViewById(R.id.editText);
+            details = dets.getText().toString();
+
+            Call<AddUserResponse> queryResponseCall =
+                    service.postMessage(comp, room, details, error, key);
+
+            queryResponseCall.enqueue(new Callback<AddUserResponse>() {
+                @Override
+                public void onResponse(Response<AddUserResponse> response) {
+                    if (response.body().getResponse().equals("ok")) {
+                        Toast.makeText(getApplicationContext(), "Error Posted To Server Sending To BELS...", Toast.LENGTH_LONG).show();
+                        rec = "aparvis@ucsc.edu";
+                        subject = "GLCS: Error Report";
+                        textMessage = "Computer: " + comp + "Room: " + room + "Error: " + error + "Details: " + details;
+                        Properties props = new Properties();
+                        props.put("mail.smtp.host", "smtp.gmail.com");
+                        props.put("mail.smtp.socketFactory.port", "465");
+                        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                        props.put("mail.smtp.auth", "true");
+                        props.put("mail.smtp.port", "465");
+
+                        session = Session.getDefaultInstance(props, new Authenticator() {
+                            protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication("glcs.ucsc@gmail.com", "Okay. What now, Lauren?");
+                            }
+                        });
+
+                        RetrieveFeedTask task = new RetrieveFeedTask();
+                        task.execute();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Please select an error type", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    // Log error here since request failed
                 }
             });
 
-            RetrieveFeedTask task = new RetrieveFeedTask();
-            task.execute();
-        }else{
-            Toast.makeText(getApplicationContext(), "Please select an error type", Toast.LENGTH_LONG).show();
-        }
 
+        }
     }
+
     class RetrieveFeedTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... params) {
 
-            try{
+            try {
                 Message message = new MimeMessage(session);
                 message.setFrom(new InternetAddress("glcs.ucsc@gmail.com"));
                 message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(rec));
                 message.setSubject(subject);
                 message.setContent(textMessage, "text/html; charset=utf-8");
                 Transport.send(message);
-            } catch(MessagingException e) {
+            } catch (MessagingException e) {
                 e.printStackTrace();
-            } catch(Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
@@ -125,5 +172,15 @@ public class ErrorReportActivity extends AppCompatActivity {
         public void onNothingSelected(AdapterView<?> parent) {
             // Another interface callback
         }
+    }
+
+    public interface PostMessageService {
+        @GET("post_message")
+        Call<AddUserResponse> postMessage(@Query("computerName") String computerName,
+                                          @Query("computerNumber") String computerNumber,
+                                          @Query("messageData") String messageData,
+                                          @Query("problem") String problem,
+                                          @Query("key") String key
+        );
     }
 }
